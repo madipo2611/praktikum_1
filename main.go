e5fc5ccc358e6d87a6bbf1bf38b91977d38659a1
madipo2611 <klimov.aka@yandex.ru>
2025-02-20 03:28:38 +0300

    v1.2. Добавил БД

--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"html/template"
 	"log"
+	_ "modernc.org/sqlite"
 	"net/http"
 )
 
 type Users struct {
-	login    string
-	password string
+	ID       int
+	Login    string
+	Password string
 }
 
-var users = map[int][]Users{}
+type Cards struct {
+	ID       int
+	Title    string
+	Subtitle string
+	Text     string
+}
+
+var db *sql.DB
 
 func main() {
-	users[1] = append(users[1], Users{login: "admin", password: "admin"})
-	users[2] = append(users[2], Users{login: "admin2", password: "admin2"})
+	var err error
+	db, err = sql.Open("sqlite", "jurry")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
 	mux.HandleFunc("/home", home)
@@ -26,7 +42,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("static/"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
 
@@ -39,6 +55,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
@@ -52,6 +69,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
@@ -65,17 +83,41 @@ func login(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
+
 	if r.Method != http.MethodPost {
-		ts.Execute(w, nil)
+		err = ts.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", 500)
+		}
+		return
 	}
+
 	username := r.FormValue("username")
 	password := r.FormValue("password")
-	if (username == "admin" && password == "admin") || (username == "admin2" && password == "admin2") {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		http.Redirect(w, r, "/home", http.StatusSeeOther)
+
+	log.Printf("Попытка входа: Логин: %s, Пароль: %s", username, password)
+
+	var user Users // Объявляем переменную user для хранения данных пользователя
+
+	err = db.QueryRowContext(r.Context(), "SELECT id FROM users WHERE Login = ? AND Password = ?", username, password).Scan(&user.ID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println("Пользователь не найден:", username)
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		} else {
+			log.Println("Ошибка при выполнении запроса:", err)
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
 	}
+
+	// Если пользователь найден и данные верные, перенаправляем на главную страницу
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
@@ -83,12 +125,36 @@ func register(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
-	err = ts.Execute(w, nil)
+	if r.Method != http.MethodPost {
+		err = ts.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", 500)
+		}
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	var exists bool
+	err = db.QueryRowContext(context.Background(), "SELECT EXISTS(SELECT 1 FROM users WHERE Login = ?)", username).Scan(&exists)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		return
 	}
+
+	if exists {
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
+		return
+	}
+
+	// Если логин не существует, добавляем нового пользователя
+	_, err = db.ExecContext(context.Background(), "INSERT INTO users (Login, Password) VALUES (?, ?)", username, password)
+	if err != nil {
+		return
+	}
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func create_card(w http.ResponseWriter, r *http.Request) {
@@ -96,10 +162,25 @@ func create_card(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
-	err = ts.Execute(w, nil)
+	if r.Method != http.MethodPost {
+		err = ts.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", 500)
+		}
+		return
+	}
+
+	title := r.FormValue("title")
+	subtitle := r.FormValue("subtitle")
+	text := r.FormValue("text")
+
+	_, err = db.ExecContext(context.Background(), "INSERT INTO cards (Title, Subtitle, Text) VALUES (?, ?, ?)", title, subtitle, text)
 	if err != nil {
 		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		return
 	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
